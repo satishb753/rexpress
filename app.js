9baf94119c94a8c86e89e72e02edcd472f3e9e74
satishb753 <satishb753@gmail.com>
2021-12-31 21:13:07 +0530

    updated production script --> npm run start:prod

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
-dotenv.config({path:'./dev-data/config.env'});
+dotenv.config({path:'./config.env'});
 const app = express();
 
-console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV==="development"){
+    // Logger Middleware ____ Only inside development environment
+    app.use(morgan('dev'));
+}
+else{
+    console.log('environment set to production');
+}
 
 export default app;
--- index.js
+++
@@ -1,7 +0,0 @@
-import app from './app';
-
-app.listen(5080, ()=>console.log("App is running on port 5001"));
-
-console.log(app.get('env'));
-
-
